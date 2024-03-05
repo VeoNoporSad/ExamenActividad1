11fef2a60314ecb39629bb375f880e7d49b0f4c5
VeoNoporSad <149404902+VeoNoporSad@users.noreply.github.com>
2024-03-05 19:27:11 -0400

    pp

--- ActividadUno.py
+++ ActividadUno.py
@@ -15,3 +15,23 @@ print("no")
 print("Modificado por MARmmmmmmCO")
 
 print("aaaaaaaaaaaaaaaaaaaa")
+
+
+
+print("hola")
+print("Adios")
+print("Adios")
+print("Adios")
+print("hola")
+print("Adios")
+print("Adios")
+print("Adios")
+print("hola")
+print("Adios")
+print("Adios")
+print("Adios")
+
+print("no")
+print("Modificado por MARmmmmmmCO")
+
+print("aaaaaaaaaaaaaaaaaaaa")
